310cf422696b1ee87f6ed1920e3999798dd38245
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-11 02:44:44 +0000

    Added feature; admin can create and delete categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,38 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    begin
+      @category.destroy
+      flash[:notice] = 'Category deleted!'
+    rescue => e
+      flash[:alert] = 'You can\'t delete a category assigned to a product'
+    end
+    redirect_to [:admin, :categories]
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,15 @@ class Category < ActiveRecord::Base
 
   has_many :products
 
+  before_destroy :check_product_number
+
+  private
+
+  def check_product_number
+    if products.any?
+        errors.add(:base, "You can't delete a category assigned to a product")
+        throw :abort
+    end
+  end
+
 end
